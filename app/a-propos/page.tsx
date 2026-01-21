8e775c7578e1eda5ca0316b530597b81fb6b9bff
jarrah-web <diengmamediarra372@gmail.com>
2026-01-21 13:29:02 +0000

    feat:creation de la page apropos

---
+++ app/a-propos/page.tsx
@@ -0,0 +1,18 @@
+export default function aapropospage() {
+    return (
+        <section className="mx-auto max-w-5xl px-6 py-16">
+            <h1 className="mb-8 text-3xl md:text-4xl font-bold">
+                À propos de moi
+            </h1>
+            <p className="mb-6 text-slate-300">
+                Je m'appelle Jarrah, un étudiant en Licence 3 de Conception de Systèmes d’Information à Dakar. Passionné par le développement web et les nouvelles technologies,
+            </p>
+            <p className="mb-6 text-slate-300">
+               J'aime collaborer avec d'autres étudiants sur des projets innovants et participer à des hackathons pour mettre mes compétences à l'épreuve.
+            </p>
+            <p className="mb-6 text-slate-300">
+                Mon objectif est de devenir une développeuse web compétente et polyvalent, capable de concevoir et de développer des solutions technologiques efficaces qui répondent aux besoins des utilisateurs. Je suis toujours à la recherche de nouvelles opportunités pour apprendre et grandir en tant que professionnel.
+            </p>
+        </section>
+    );
+}
